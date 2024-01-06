ff96beb27ceb14d7e841c852b967c669b63f5206
szramkalukasz <lukasz_szramka@wp.pl>
2024-01-06 10:53:47 +0100

    Iadded main content and styled it

--- front-end/src/components/Layout/Layout.jsx
+++ front-end/src/components/Layout/Layout.jsx
@@ -3,21 +3,25 @@ import { CurrencySelector } from "../CurrencySelector/CurrencySelector";
 import { Footer } from "../Footer/Footer";
 import { IconMenu } from "../IconMenu/IconMenu";
 import { Logo } from "../Logo/Logo";
+import { MainContent } from "../MainContent/MainContent";
 import { MainMenu } from "../MainMenu/MainMenu";
 import { TopBar } from "../TopBar/TopBar";
 
-export function Layout() {
+export function Layout({ children }) {
   return (
     <>
-      <TopBar>
-        <MainMenu />
-        <Logo />
-        <div>
-          <CurrencySelector />
-          <IconMenu />
-        </div>
-      </TopBar>
-      <CategoryMenu />
+      <MainContent>
+        <TopBar>
+          <MainMenu />
+          <Logo />
+          <div>
+            <CurrencySelector />
+            <IconMenu />
+          </div>
+        </TopBar>
+        <CategoryMenu />
+        {children}
+      </MainContent>
       <Footer />
     </>
   );
