704f6e707171481a65208a979f3891e4c358d375
szramkalukasz <lukasz_szramka@wp.pl>
2024-01-21 11:05:03 +0100

    product list integration

--- front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
+++ front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
@@ -1,19 +1,26 @@
 import styles from "./ExpandableMenu.module.css";
-
 import { CATEGORIES } from "../../constants/categories";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import ARROW_ICON from "../../assets/arrow.svg";
 
+const PATH_TO_GENDER_NAME = {
+  kobieta: "Kobieta",
+  mezczyzna: "Mężczyzna",
+  dziecko: "Dziecko",
+};
+
 export function ExpandableMenu() {
-  const activePath = "odziez";
+  const params = useParams();
+
+  const activePath = params.category;
   return (
     <div className={styles.expandableMenu}>
-      <p>Kobieta</p>
+      <p>{PATH_TO_GENDER_NAME[params.gender]}</p>
       <ul>
         {CATEGORIES.map((category) => {
           return (
             <li key={category.path}>
-              <NavLink to={category.path}>
+              <NavLink to={`/${params.gender}/${category.path}`}>
                 {category.categoryName}{" "}
                 <img
                   src={ARROW_ICON}
@@ -27,7 +34,9 @@ export function ExpandableMenu() {
                   {category.subcategories.map((subcategory) => {
                     return (
                       <li key={subcategory.path}>
-                        <NavLink to={subcategory.path}>
+                        <NavLink
+                          to={`/${params.gender}/${params.category}/${subcategory.path}`}
+                        >
                           {subcategory.categoryName}
                         </NavLink>
                       </li>
