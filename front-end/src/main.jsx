b49412cb7691c28202a113fb075b8faa13e1e2f6
szramkalukasz <lukasz_szramka@wp.pl>
2024-01-30 20:10:00 +0100

    adding and removing from the favorites list

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -14,11 +14,17 @@ import { ProductDetails } from "./views/ProductDetails/ProductDetails";
 import { productListLoader } from "./api/productListLoader";
 import { productLoader } from "./api/productLoader";
 import { addProductToFavourites } from "./api/addProductToFavouritesAction";
+import { favouritesLoader } from "./api/favouritesLoader";
+import { deleteFavouriteAction } from "./api/deleteFavouriteAction";
 
 const router = createBrowserRouter([
   {
     path: "/add-to-favourites/:productId",
     action: addProductToFavourites,
+  },
+  {
+    path: "/delete-from-favourites/:favouriteId",
+    action: deleteFavouriteAction,
   },
   {
     path: "",
@@ -31,6 +37,7 @@ const router = createBrowserRouter([
       {
         path: "/ulubione",
         element: <Favourites />,
+        loader: favouritesLoader,
       },
       {
         path: "/:gender?",
